1517ac1b5a863ecd6f99cbfdeebab89069249c7f
Jiří Šitina <jiri.sitina@gooddata.com>
2024-02-05 21:14:09 +0100

    Draft of forecast

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import pickle
 import json
 import random
-from jinja2 import TemplateAssertionError
+# from jinja2 import TemplateAssertionError
 import requests
 import time
 
@@ -16,6 +16,12 @@ from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from datetime import datetime
 from dateutil import parser
+import matplotlib.pyplot as plt
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 album = ''
 app = Flask(__name__)
@@ -77,8 +83,7 @@ def get_random_picture():
 def get_weather():
     ts = time.time()
     if ts - weather_cache['ts'] > 300:
-        # print('fetching fresh weather data')
-        if (weather_api_key):
+        if weather_api_key:
             weather_url = f"http://api.openweathermap.org/data/2.5/weather?appid={weather_api_key}&q={weather_location}"
             weather_data = requests.get(weather_url).json()
             weather_cache['ts'] = ts
@@ -89,9 +94,104 @@ def get_weather():
         return weather_cache['data']
 
 
+def get_forecast():
+    print('getting forecast')
+    forecast_file = 'forecast.json'
+    lon = 14.4936
+    lat = 50.1267
+    ts = time.time()
+    if ts - weather_cache['forecast_ts'] > 300:
+        # print('fetching fresh weather data')
+        if os.path.isfile(forecast_file):
+            with open(forecast_file) as f:
+                return json.load(f)
+            # print('fetching fresh weather data')
+        elif weather_api_key:
+            print('getting forecast')
+            weather_url = f"http://api.openweathermap.org/data/2.5/forecast?appid={weather_api_key}&lat={lat}&lon={lon}"
+            weather_data = requests.get(weather_url).json()
+            weather_cache['forecast_ts'] = ts
+            weather_cache['forecast'] = weather_data
+            return weather_data
+    else:
+        # print('using weather cache')
+        return weather_cache['forecast']
+
+
 @app.route("/")
 def home():
     return get_fullscreen()
+
+
+def to_celsius(original):
+    return round(original - 273.15, 1)
+
+
+def process_forecast(forecast_data):
+    print(forecast_data)
+    result = []
+    for item in forecast_data['list']:
+        temp = to_celsius(item['main']['temp'])
+        feels_like = to_celsius(item['main']['feels_like'])
+        humidity = item['main']['humidity']
+        weather = item['weather'][0]['main']
+        dt = item['dt_txt']
+        result.append(
+            {
+                'dt': dt,
+                'temp': temp,
+                'feels_like': feels_like,
+                'weather': weather,
+                'humidity': humidity,
+            }
+        )
+    return result
+
+
+@app.route('/weather/forecast.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    # fig.title('title name')
+    # fig.xlabel('x_axis name')
+    # fig.ylabel('y_axis name')
+
+    forecast_data = get_forecast()
+    forecast = process_forecast(forecast_data)
+
+    xs = [f['dt'] for f in forecast]
+    ys = [f['temp'] for f in forecast]
+
+    # xs = range(100)
+    # ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+
+
+@app.route("/weather")
+def weather_view():
+    weather = get_weather()
+    forecast_data = get_forecast()
+    forecast = process_forecast(forecast_data)
+    print(forecast)
+    temperature = to_celsius(weather['main']['temp'])
+    feels_like = to_celsius(weather['main']['feels_like'])
+    weather_type = weather['weather'][0]['main']
+
+    return render_template(
+        'weather.html',
+        temperature=temperature,
+        feels_like=feels_like,
+        weather_type=weather_type,
+        forecast=forecast
+    )
 
 
 @app.route("/albums")
@@ -142,7 +242,7 @@ if os.path.exists("token.pickle"):
     with open("token.pickle", "rb") as tokenFile:
         creds = pickle.load(tokenFile)
 if not creds or not creds.valid:
-    if (creds and creds.expired and creds.refresh_token):
+    if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
@@ -159,7 +259,7 @@ try:
         album = config['album']
         weather_api_key = config['weather_api_key']
         weather_location = config['weather_location']
-        weather_cache = {'ts': 0}
+        weather_cache = {'ts': 0, 'forecast_ts': 0}
         pictures_cache = {'ts': 0}
         print(album)
 except FileNotFoundError:
